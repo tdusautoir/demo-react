845891f93726773a8a3f6fd3cc6a6737b535e4f2
Thibaut Dusautoir <tdusautoir@myges.fr>
2023-01-15 19:39:05 +0100

    ajout affichage tableau et detail card

--- src/components/IdentityCard.js
+++ src/components/IdentityCard.js
@@ -1,20 +1,26 @@
+import { useState } from "react";
+
 const IdentityCard = ({ person, onDelete }) => {
-    console.log(person);
+
+    const [showDetail, setShowDetail] = useState(0);
 
     return (
         <div className="card">
             <img src={person.picture.large} alt={person.picture.large} />
             <p>{person.name.first}</p>
             <p>{person.name.last}</p>
-            <button onClick={() => {onDelete(person.login.uuid)}}>Supprimer</button>
-            {/* <div className="detail" >
+            <div className="buttons">
+                <button onClick={() => {onDelete(person.login.uuid)}}>Supprimer</button>
+                <button onClick={() => { showDetail ? setShowDetail(0) : setShowDetail(1) }}>...</button>
+            </div>
+            <div className="detail" style={showDetail ? { display: 'block' } : { display: 'none'}}>
                 <p>Nom : </p>
                 <p>{person.name.last}</p>
                 <p>Genre : </p>
                 <p>{person.gender === 'fermale' ? 'Femme' : 'Homme'}</p>
                 <p>Date : </p>
                 <p>{new Date(person.dob.date).toLocaleDateString()}</p>
-            </div> */}
+            </div>
         </div>
     );
 }
--- src/components/NameGenerator.js
+++ src/components/NameGenerator.js
@@ -3,14 +3,14 @@ import IdentityCard from "./IdentityCard";
 
 const NameGenerator = () => {
     const [persons, setPersons] = useState([]);
-    const [dataType, setDataType] = useState(1);
+    const [dataType, setDataType] = useState(0);
 
     const addNewPerson = async () => {
         const response = await fetch("https://randomuser.me/api/");
         const result = await response.json();
 
         const newPerson = result.results[0];
-        setPersons(persons.concat([newPerson]));
+        setPersons([...persons, newPerson]);
     }
 
     const deletePerson = (id) => {
@@ -20,9 +20,10 @@ const NameGenerator = () => {
     return <>
         <h1>Génerateur de nom</h1>
         <button onClick={addNewPerson}>Nouvelle ligne</button>
-        <select onChange={(e) => setDataType(parseInt(e.target.value))}>
+        <select onChange={(e) => setDataType(parseInt(e.target.value))} defaultValue="0">
             <option value="0">card</option>
-            <option value="1" selected>csv</option>
+            <option value="1">csv</option>
+            <option value="2">Tableau</option>
         </select>
 
         {dataType === 0 && (
@@ -39,6 +40,33 @@ const NameGenerator = () => {
                 {persons.map((person) => (
                     <p key={person.login.uuid}>{person.name.first + ',' + person.name.last + ',' + new Date(person.dob.date).toLocaleDateString() + ',' + (person.gender === 'female' ? 'Femme' : 'Homme')}</p>
                 ))}
+            </div>
+        )}
+
+        {dataType === 2 && ( 
+            <div className="tab-container container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Prénom</th>
+                            <th>Nom</th>
+                            <th>Date</th>
+                            <th>Genre</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {persons.map((person) => {
+                        return (
+                            <tr key={person.login.uuid}>
+                                <td>{person.name.first}</td>
+                                <td>{person.name.last}</td>
+                                <td>{new Date(person.dob.date).toLocaleDateString()}</td>
+                                <td>{person.gender === 'female' ? 'Femme' : 'Homme'}</td>
+                            </tr>
+                        );
+                        })}
+                    </tbody>
+                </table>
             </div>
         )}
     </>
